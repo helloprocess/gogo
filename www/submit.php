1edcdedf4d93db935521439f8f8b24fb7fc6c87b
Rodo Lillo <rl@owit.eu>
2025-03-21 12:42:29 +0100

    v1.0

--- www/submit.php
+++ www/submit.php
@@ -1,5 +1,5 @@
 <?php
 header("Status: 301 Moved Permanently");
-header('Location: http://kkkk' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/submit?' . $_SERVER['QUERY_STRING']);
+header('Location: http://' . $_SERVER['SERVER_NAME'] . '/submit?' . $_SERVER['QUERY_STRING']);
 exit();
 ?>
